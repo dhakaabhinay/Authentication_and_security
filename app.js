4487e2be5936de4ea3a03e2a65cb651f5deee50d
dhakaabhinay <101544134+dhakaabhinay@users.noreply.github.com>
2022-04-03 22:00:36 +0530

    commit for passport

--- app.js
+++ app.js
@@ -6,9 +6,12 @@ const ejs = require("ejs");
 const mongoose = require("mongoose");
 // const encrypt = require("mongoose-encryption");
 // const md5 = require("md5");
-const bcrypt = require('bcryptjs');
-
-const salt = bcrypt.genSaltSync(10);
+// const bcrypt = require('bcryptjs');
+//
+// const salt = bcrypt.genSaltSync(10);
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose= require('passport-Local-Mongoose');
 
 const app = express();
 
@@ -18,6 +21,16 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+app.use(session({
+  secret: 'abhinay Use Secret Key',
+  resave: false,
+  saveUninitialized: false
+//  cookie: { secure: true }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.connect("mongodb://localhost:27017/userDB");
 
 const userSchema =  new mongoose.Schema ( {
@@ -25,12 +38,23 @@ const userSchema =  new mongoose.Schema ( {
   password:String
 });
 
+
+userSchema.plugin(passportLocalMongoose);
+
 // const secret = process.env.SECRETS;
 //
 // userSchema.plugin(encrypt, { secret: secret ,encryptedFields: ['password']});
 
 
 const usermodel = new mongoose.model("user" , userSchema);
+
+
+passport.use(usermodel.createStrategy());
+
+// use static serialize and deserialize of model for passport session support
+passport.serializeUser(usermodel.serializeUser());
+passport.deserializeUser(usermodel.deserializeUser());
+
 
 app.get("/",function(req,res){
   res.render("home")
@@ -40,48 +64,90 @@ app.get("/login",function(req,res){
   res.render("login")
 })
 
+app.get("/logout",function(req,res){
+  req.logout();
+  res.redirect("/")
+})
 app.get("/register",function(req,res){
   res.render("register")
 })
 
-app.post("/register", function(req ,res){
-  const newUser = new usermodel({
-    email:req.body.username,
-    password: bcrypt.hashSync(req.body.password , salt)
-  });
-  newUser.save(function(err){
-    if(err){
-      res.render(err);
-    }else{
-      res.render("Secrets");
+app.get("/Secrets" , function(req , res){
+  if(req.isAuthenticated ())
+  {
+    res.render("Secrets");
+  }
+  else{
+    res.redirect("/login");
+  }
+})
 
-    }
-  });
+app.post("/register", function(req ,res){
+
+  usermodel.register({username:req.body.username} , req.body.password , function(err , user){
+if(err){
+  console.log(err);
+  res.redirect("/register")
+}else{
+  passport.authenticate("local")(req,res ,function(){
+    res.redirect("/Secrets")
+  })
+}
+
+  })
+  // const newUser = new usermodel({
+  //   email:req.body.username,
+  //   password: bcrypt.hashSync(req.body.password , salt)
+  // });
+  // newUser.save(function(err){
+  //   if(err){
+  //     res.render(err);
+  //   }else{
+  //     res.render("Secrets");
+  //
+  //   }
+  // });
 });
 
 
 app.post("/login", function(req ,res){
 
-  const username = req.body.username;
-  //const   password =  bcrypt.compareSync(req.body.password);
-usermodel.findOne({emai:username},function(err ,finduser){
-if(err){
+
+const user = new usermodel({
+  username: req.body.username,
+  password:  req.body.password
+});
+
+req.login(user,function(err){
+  if(err){
   console.log(err);
-}  else{
-
-  if(finduser){
-
-  if (bcrypt.compareSync(req.body.password, finduser.password)) {
-
-
-      res.render("Secrets")
-    }
-    else{
-      res.send("wrong username or password")
-    }
-  }
+  res.send("wrong username or password")
+}else{
+  passport.authenticate("local")(req,res ,function(){
+    res.redirect("/Secrets")
+  })
 }
 })
+  //const username = req.body.username;
+  //const   password =  bcrypt.compareSync(req.body.password);
+// usermodel.findOne({emai:username},function(err ,finduser){
+// if(err){
+//   console.log(err);
+// }  else{
+//
+//   if(finduser){
+//
+//   if (bcrypt.compareSync(req.body.password, finduser.password)) {
+//
+//
+//       res.render("Secrets")
+//     }
+//     else{
+//       res.send("wrong username or password")
+//     }
+//   }
+// }
+// })
 
 });
 
